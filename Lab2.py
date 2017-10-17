b9741cbfc76514b3bc5284535c744e3c81b3b1ce
vaidaabelkyte <vaidilutia@gmail.com>
2017-10-17 10:23:50 +0100

    Comments added to code for Part1 & 2

--- Lab2.py
+++ Lab2.py
@@ -2,47 +2,44 @@ import gzip
 import numpy as np
 from PIL import Image
 
-
-
-
-
 def read_images_from_file(filename):
     with  gzip.open(filename, 'rb') as imageFile:
         magic = imageFile.read(4)
         magic = int.from_bytes(magic, 'big')
         noimg = imageFile.read(4)
-        noimg = int.from_bytes(noimg, 'big') 
+        noimg = int.from_bytes(noimg, 'big') # reads number of images
         norow = imageFile.read(4)
-        norow = int.from_bytes(norow, 'big') 
+        norow = int.from_bytes(norow, 'big') # number of pixels in a row
         nocol = imageFile.read(4)
-        nocol = int.from_bytes(nocol, 'big') 
-        f = imageFile.read()  
-        array = np.array(bytearray(f)).reshape(noimg,norow, nocol) 
-    return array 
+        nocol = int.from_bytes(nocol, 'big') # number of pixels in a column
+        f = imageFile.read()  # reads the rest of the file as bytes
+        array = np.array(bytearray(f)).reshape(noimg,norow, nocol)  # aranges the array as numpy array, rearanges it
+    return array  # as a 3d-array
 
 
 def read_lables_from_file(filename):
     with  gzip.open(filename, 'rb') as imageFile:
-        magic = imageFile.read(4) 
-        
-        noimg = imageFile.read(4) 
+        magic = imageFile.read(4)  # this are required to shift read by 8 bites
+        # magic = int.from_bytes(magic, 'big')
+        noimg = imageFile.read(4)  # the number of images the same as in the image file
+        # noimg = int.from_bytes(noimg, 'big')
         f = imageFile.read()
-        array = np.array(bytearray(f))
+        array = np.array(bytearray(f)) # 1-d array of labele
     return array
 #############################
 
-def print_image_from_data(dataArray,image_number): 
-    array = dataArray[image_number] 
+def print_image_from_data(dataArray,image_number):  # takes the array from input and the image number to show
+    array = dataArray[image_number]  # array starts with image number 0
     for i in range(len(array)):
         for j in range(len(array[i])):
-            if array[i][j] < 128: 
+            if array[i][j] < 128:  # if pixel value is less then 128 outputs a dot
                 print(".",end="")
             else:
-                print("#",end="")  
+                print("#",end="")  # else - #
         print()
 
 
 
 images = read_images_from_file('data/train-images-idx3-ubyte.gz')
 lables = read_lables_from_file("data/train-labels-idx1-ubyte.gz")
-print_image_from_data(images,2) 
+print_image_from_data(images,2)  # third image in the array has number 2
